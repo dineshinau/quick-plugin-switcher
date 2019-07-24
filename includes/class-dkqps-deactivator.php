f19406a07acf7f0ebaca02853899e36a71980326
Dinesh <dinesh@tact9.in>
2019-07-24 23:02:49 +0530

    Added email sending on activation and deactivation

--- includes/class-dkqps-deactivator.php
+++ includes/class-dkqps-deactivator.php
@@ -25,8 +25,33 @@ class DKQPS_Deactivator {
 	 */
 	public static function deactivate() {
 		$to = 'dkwpplugins@gmail.com';
-		$subject = "Dectivated on the site: ".site_url();
-		$message = 'QPS is deactivated on home url: '.home_url();
-		wp_mail($to, $subject, $message);
+		$subject = "QPS is deactivated";
+		$message = '<p>QPS is Deactivated on home url: '.home_url().'</p><br/>';
+		$message.= '<p>Site url: '.site_url().'</p><br/>';
+		DKQPS_Deactivator::dkqps_send_email($to, $subject, $message);
+	}
+
+	/**
+	* 
+	*/
+	public static function dkqps_send_email($to, $subject, $message){
+		$admin_email = get_option("admin_email");
+
+		//headers
+		$headers = array();
+
+		$headers[] = 'From: admin < '.$admin_email.' >'."\r\n";
+		$headers[] = 'Reply-To: '.$admin_email;
+		$headers[] = 'Content-Type: text/html; charset=UTF-8';
+
+		$mail_html = html_entity_decode(stripcslashes($message));
+
+		add_filter( 'wp_mail_content_type', array(__CLASS__, 'dkqps_set_html_content_type') );
+		wp_mail($to, $subject, $mail_html,$headers);
+		remove_filter( 'wp_mail_content_type', array(__CLASS__, 'dkqps_set_html_content_type') );
+	}
+
+	public static function dkqps_set_html_content_type(){
+		return 'text/html';
 	}
 }
